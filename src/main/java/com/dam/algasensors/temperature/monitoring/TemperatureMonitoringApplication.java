ff5386fc241c9bdb2edb3de84f4ce8015b9d1d4f
Douglas Moraes <douglas.am@gmail.com>
2025-05-30 23:42:56 -0300

    New project files

---
+++ src/main/java/com/dam/algasensors/temperature/monitoring/TemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.dam.algasensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMonitoringApplication.class, args);
+	}
+
+}
